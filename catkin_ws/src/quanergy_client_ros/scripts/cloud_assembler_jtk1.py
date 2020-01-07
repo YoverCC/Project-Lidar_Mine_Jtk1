54a5850eb62d028082f7ac01b40d2f506f0ceb83
YoverCC <ycastroc@uni.pe>
2020-01-07 13:12:28 -0500

    Ultimos cambios agregados - Stack problem

--- catkin_ws/src/quanergy_client_ros/scripts/cloud_assembler_jtk1.py
+++ catkin_ws/src/quanergy_client_ros/scripts/cloud_assembler_jtk1.py
@@ -16,45 +16,42 @@ import sys
 time_init = None
 time_end = None
 captura_3d = False
-count_init = 0
-count_end = 0
+prev_value = 0
 
 
 def callback(data):
-	global time_init, time_end, captura_3d, count_init, count_end
-	value = data.data[6:9]
-	rospy.loginfo(value)
-	if int(value) == 180:
+	global time_init, time_end, captura_3d, prev_value
+	value = int(data.data[6:9])
+	rospy.loginfo("Angulo en servo: %d",value)
+	dif = value - prev_value
+	if value==170 and dif>0:
 		time_init = rospy.get_rostime()
-		count_init = count_init + 1
-
-	if int(value) == 5:
-		captura_3d = False
-		count_end = 0
+		print "Start stack"
 		
-	if int(value) == 0:
-		if count_init > 0:
-			count_end = count_end + 1
-			time_end = rospy.get_rostime()
-	if count_end > 0:
+	if value==0:
+		time_end = rospy.get_rostime()
 		captura_3d = True
+		print "Finish stack"
+	prev_value = value
+
+		
 
 
 def assembler_client():
-	global time_init, time_end, captura_3d, count_init, count_end, path_to_folder
+	global time_init, time_end, captura_3d, prev_value
 	rospy.init_node("assembler_client")
 	rospy.wait_for_service("assemble_scans2")
 	assemble_scans = rospy.ServiceProxy("assemble_scans2", AssembleScans2)
 	pub = rospy.Publisher("/assembled_cloud", PointCloud2, queue_size = 10)
-	r = rospy.Rate(5) # El periodo debe ser menor al periodo del arduino en el envio del angulo al procesador central
+	r = rospy.Rate(10) # El periodo debe ser menor al periodo del arduino en el envio del angulo al procesador central
 	rospy.Subscriber("servo_angle", String, callback)
 
 	while not rospy.is_shutdown():
 		if captura_3d:
 			try:
-				resp = assemble_scans(time_init,time_end) # time_init, time_end
-				print "Got cloud"
+				resp = assemble_scans(rospy.Time(0,0),time_end) # time_init, time_end
 				pub.publish(resp.cloud)
+				print "Got cloud"
 				captura_3d = False
 
 			except rospy.ServiceException, e:
